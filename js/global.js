c23e2602059d579e8ad05acfa7dafbe27893f52c
Aleksandr Yaremenko <mymusicdisc1@gmail.com>
2016-05-11 16:52:05 +0300

    no message

---
+++ js/global.js
@@ -0,0 +1,8 @@
+﻿$(document).ready(function(){
+	$('#top_menu').on('click', function(){
+		$('.var-menu').toggleClass('show');
+			});
+	$('.outer > li').on('click', function(){
+		$(this).find('.inner').css('display', 'block');
+			});
+		});
